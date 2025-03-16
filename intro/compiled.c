a090a22bc3428de608b7bc63c82b997936fa65d9
VArtz <farrelnikoson@gmail.com>
2025-03-17 00:29:53 +0700

    tidy up

--- intro/compiled.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-  printf("starting sneklang tools\n");
-  printf("finished sneklang tools\n");
-  return 0;
-}
--- intro/intro.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Starting the Sneaklang interpreter...\n");
-    return 0;
-}
